07e314093adfdf8285a15cf9023eb872176bcf39
zaytsev <zaicev@mniti.ru>
2024-07-04 15:52:54 +0300

    added useState and removeTask

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
 import './App.css';
-import {Todolist} from "./Todolist";
+import {TasksType, Todolist} from "./Todolist";
+import { v4 as uuidv4 } from 'uuid';
+import {useState} from "react";
 
 function App() {
+
+    let [tasks , setTasks] = useState<Array<TasksType>>([
+        {id: uuidv4(), title: "HTML", isDone: true },
+        {id: uuidv4(), title: "CSS", isDone: true },
+        {id: uuidv4(), title: "ES6/TS", isDone: false },
+    ])
+
+    const removeTask = (taskId: string) => {
+        console.log("deleted task")
+        tasks = tasks.filter(t => t.id !== taskId)
+        setTasks(tasks)
+    }
+
     return (
         <div className="App">
-            <Todolist title="What to learn"/>
-            <Todolist title="Songs"/>
-            <Todolist title="Books"/>
+            <Todolist title="What to learn" tasks={tasks} removeTask={removeTask}/>
+           {/* <Todolist title="What to buy" tasks={tasks_2}/>*/}
         </div>
     );
 }
--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,27 +1,48 @@
 import './App.css';
 import {Button} from "./Button";
 
-interface TodolistType {
-    title: string
+export interface TasksType {
+    id: string,
+    title: string,
+    isDone: boolean
 }
-export const Todolist = ({title}: TodolistType) => {
 
+interface TodolistType {
+    title: string,
+    tasks: Array<TasksType>,
+    removeTask: (taskId: string) => void
+}
+
+export const Todolist = ({title, tasks, removeTask}: TodolistType) => {
+    console.log(tasks)
     return (
         <div className="todolist">
             <h3>{title}</h3>
-            <div >
-                <input/>
-                <Button name="+" callback={()=> {}}/>
-            </div>
-            <ul>
-                <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                <li><input type="checkbox" checked={false}/> <span>React</span></li>
-            </ul>
             <div>
-                <Button name="All" callback={()=> {}}/>
-                <Button name="Active" callback={()=> {}}/>
-                <Button name="Completed" callback={()=> {}}/>
+                <input/>
+                <Button name="+" callback={() => {
+                }}/>
+            </div>
+            {tasks.length === 0 ? (
+                <p>Тасок нет</p>
+            ) : (
+                <ul>
+                    {tasks.map(task => (
+                        <li key={task.id}>
+                            <input type="checkbox" checked={task.isDone}/>
+                            <span>{task.title}</span>
+                            <Button name="X" callback={() =>removeTask(task.id)}/>
+                        </li>
+                    ))}
+                </ul>
+            )}
+            <div>
+                <Button name="All" callback={() => {
+                }}/>
+                <Button name="Active" callback={() => {
+                }}/>
+                <Button name="Completed" callback={() => {
+                }}/>
             </div>
         </div>
     );
