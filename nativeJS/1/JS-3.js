abda00855a26c9d7a7e476d5d92ff867afeed60f
zaytsev <zaicev@mniti.ru>
2024-07-11 16:46:41 +0300

    продолжение

--- nativeJS/1/JS-3.js
+++ nativeJS/1/JS-3.js
@@ -3,14 +3,14 @@ const md = document.getElementById('medium')
 const bg = document.getElementById('big')
 
 const handlerSM= (event) => {
-    console.log(1)
+    console.log(event)
 }
 
-const handlerMD = () => {
-    console.log(2)
+const handlerMD = (event) => {
+    console.log(event)
 }
-const handlerBG = () => {
-    console.log(3)
+const handlerBG = (event) => {
+    console.log(event)
 }
 //Вызов функции во время клика на <div id="small">
 /*
@@ -26,5 +26,5 @@ sm.removeEventListener('click', handlerSM2)
 */
 
 sm.addEventListener('click', handlerSM, false)
-md.addEventListener('click', handlerMD,{capture: true}) //вызов на стадии погружения
+md.addEventListener('click', handlerMD,{capture: false}) //вызов на стадии погружения
 bg.addEventListener('click', handlerBG, false) 
