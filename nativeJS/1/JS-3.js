1ad107b29562f5655f6e9a30a5056bc3ca1322d5
zaytsev <zaicev@mniti.ru>
2024-07-12 11:27:49 +0300

    продолжение

--- nativeJS/1/JS-3.js
+++ nativeJS/1/JS-3.js
@@ -3,15 +3,16 @@ const md = document.getElementById('medium')
 const bg = document.getElementById('big')
 
 const handlerSM= (event) => {
-    console.log(event)
+    console.log(event.currentTarget)
+    event.currentTarget.id === "small" ? alert("small"): ''
 }
 
-const handlerMD = (event) => {
-    console.log(event)
+/*const handlerMD = (event) => {
+    console.log(event.currentTarget)
 }
 const handlerBG = (event) => {
-    console.log(event)
-}
+    console.log(event.currentTarget)
+}*/
 //Вызов функции во время клика на <div id="small">
 /*
 sm.onclick = handlerSM1 //не сработает
@@ -28,3 +29,9 @@ sm.removeEventListener('click', handlerSM2)
 sm.addEventListener('click', handlerSM, false)
 md.addEventListener('click', handlerMD,{capture: false}) //вызов на стадии погружения
 bg.addEventListener('click', handlerBG, false) 
+
+
+//Метод addEventListener позволяет 
+// 1) удобно вешать несколько событий(обработчиков) на один элемент
+// 2) Обработчики не хранятся вместе с элементов. Его удобнее удалять 
+// 3) Он предоставляет объект с опциями для управления обработки событий
