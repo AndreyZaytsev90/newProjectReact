5a77a0afd883d5136e77d1016947f82d299176a3
zaytsev <zaicev@mniti.ru>
2024-07-12 14:49:56 +0300

    завершение 3 урока

--- nativeJS/1/JS-3.js
+++ nativeJS/1/JS-3.js
@@ -4,15 +4,18 @@ const bg = document.getElementById('big')
 
 const handlerSM= (event) => {
     console.log(event.currentTarget)
-    event.currentTarget.id === "small" ? alert("small"): ''
+    //event.stopPropagation()
+    /*event.currentTarget.tagName === "small" ? alert("small"): ''*/
+
+    event.target.tagName === "BUTTON" ? alert(event.target.id): ''
 }
 
-/*const handlerMD = (event) => {
+const handlerMD = (event) => {
     console.log(event.currentTarget)
 }
 const handlerBG = (event) => {
     console.log(event.currentTarget)
-}*/
+}
 //Вызов функции во время клика на <div id="small">
 /*
 sm.onclick = handlerSM1 //не сработает
@@ -27,11 +30,19 @@ sm.removeEventListener('click', handlerSM2)
 */
 
 sm.addEventListener('click', handlerSM, false)
-md.addEventListener('click', handlerMD,{capture: false}) //вызов на стадии погружения
-bg.addEventListener('click', handlerBG, false) 
+/*md.addEventListener('click', handlerMD,{capture: false}) //вызов на стадии погружения
+bg.addEventListener('click', handlerBG, false) */
 
 
 //Метод addEventListener позволяет 
 // 1) удобно вешать несколько событий(обработчиков) на один элемент
 // 2) Обработчики не хранятся вместе с элементов. Его удобнее удалять 
 // 3) Он предоставляет объект с опциями для управления обработки событий
+
+const a = document.getElementById("a")
+a.addEventListener('click', a_click)
+
+function a_click(e) {
+    e.preventDefault() // отменяет действие браузера по-умолчанию
+    alert("hey")
+}
