0675ccde2760e3587d55216f0f5008e4a7b64a49
zaytsev <zaicev@mniti.ru>
2024-07-19 15:08:20 +0300

    SP1 LS4 nativeJS continues

--- nativeJS/1/JS-4.js
+++ nativeJS/1/JS-4.js
@@ -8,8 +8,8 @@ function Id() {
     return result;
 }
 
-const todolistId1 = Id()
-const todolistId2 = Id()
+const todolistId1 = '1'
+const todolistId2 = '2'
 
 const todolist = [
     {
@@ -52,23 +52,50 @@ const tasks = {
     // В JavaScript квадратные скобки [] в объявлении объекта используются для вычисляемых свойств.
     // Это означает, что значение внутри квадратных скобок будет вычислено и использовано как ключ объекта.
     [todolistId1]: [
-        {id: Id(), title: "HTML", isDone: true},
-        {id: Id(), title: "CSS", isDone: true},
-        {id: Id(), title: "ES6/TS", isDone: false},
-        {id: Id(), title: "REACT", isDone: false},
+        {id: '3', title: "HTML", isDone: true},
+        {id: '4', title: "CSS", isDone: true},
+        {id: '5', title: "ES6/TS", isDone: false},
+        {id: '6', title: "REACT", isDone: false},
     ],
     [todolistId2]: [
-        {id: Id(), title: "Milk", isDone: true},
-        {id: Id(), title: "Bread", isDone: true},
-        {id: Id(), title: "Eggs", isDone: false},
-        {id: Id(), title: "Butter", isDone: false},
+        {id: '7', title: "Milk", isDone: true},
+        {id: '8', title: "Bread", isDone: true},
+        {id: '9', title: "Eggs", isDone: false},
+        {id: '10', title: "Butter", isDone: false},
     ],
-    [12 + 58]: [], //70
-    [key]: [], // интерпритатор прочитает значение переменной key, приведет к строке результат и назначит в качестве ключа - 70
+   /* [12 + 58]: [], //70
+    [key]: [], // интерпретатор прочитает значение переменной key, приведет к строке результат и назначит в качестве ключа - 70
     [12 < 58]: [], //true
-    [Id()]: []  // результат выполнения функции Id() будет приведен к строке и передан в качестве ключа
+    [Id()]: []  // результат выполнения функции Id() будет приведен к строке и передан в качестве ключа*/
 }
 
-console.log(tasks[todolistId1])
+//console.log(tasks[todolistId1])
 
-console.log(tasks[todolistId1].filter((t)=> t.id !== 'SQntd05JEsPI'))
+/*console.log(tasks[todolistId1].filter((t)=> t.id !== '4')) //удаление таски с id 4
+console.log(tasks[todolistId2].filter((t)=> t.id !== '7')) //удаление таски с id 7*/
+
+/*
+console.log(tasks[todolistId1][3])*/
+
+
+const todolistTasks = tasks[todolistId1]
+const todolistTasks2 = tasks[todolistId2]
+
+
+const filteredTasks = todolistTasks.filter((t)=> t.id !== '4')
+const filteredTasks2 = todolistTasks2.filter((t)=> t.id !== '9')
+
+const copyTasks = {...tasks}
+copyTasks[todolistId1] = filteredTasks
+copyTasks[todolistId2] = filteredTasks2
+
+
+console.log(tasks)
+console.log(copyTasks)
+const todolistTasks3 = copyTasks[todolistId1]
+
+/*const newCopyTasks = {...copyTasks}
+const filteredTasks3 = todolistTasks3.map((t)=> t.id === '5'? {...t, isDone: true}: t)
+newCopyTasks[todolistId1] = filteredTasks3*/
+
+console.log({...copyTasks, [todolistId1]: todolistTasks3.map((t)=> t.id === '5'? {...t, "isDone": true}: t)})
