f19fb81165c0bf828e2206aa026d2975f17d3342
zaytsev <zaicev@mniti.ru>
2024-08-19 16:58:27 +0300

    SP2 LS7 todolist isDone reducers isDone

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,32 @@
-import React, {useState} from 'react';
+import React, {useReducer, useState} from 'react';
 import './App.css';
 import {Todolist} from './Todolist';
 import { v1 } from 'uuid';
-
+import {addTaskAC, removeTaskAC, tasksReducer} from "./reducers/tasksReducer";
+import {changeFilterAC, filterReducer} from "./reducers/filterReducer";
+/*
+* Рано или поздно мы столкнемся с тем, что наш проект необходимо будет масштабировать.
+* Структура нашего проекта сейчас для этого абсолютно не годится
+* Оказывается, внутри проект работает совершенно по другому.
+* Сейчас посмотрим что такое редьюсер(функция)*/
 export type FilterValuesType = "all" | "active" | "completed";
 
 function App() {
 
-    let [tasks, setTasks] = useState([
+   /* let [tasks, setTasks] = useState([
+        { id: v1(), title: "HTML&CSS", isDone: true },
+        { id: v1(), title: "JS", isDone: true },
+        { id: v1(), title: "ReactJS", isDone: false },
+        { id: v1(), title: "Rest API", isDone: false },
+        { id: v1(), title: "GraphQL", isDone: false },
+    ]);*/
+
+    /*
+    * сетает (вызывают функцию set.. из useState только школота)
+    * нормальные разрабы используют dispatch
+    * */
+
+    let [tasks, dispatchTasks] = useReducer(tasksReducer,[ // useReducer - для больших(не локальных) state (может использоваться вместе с Redux)
         { id: v1(), title: "HTML&CSS", isDone: true },
         { id: v1(), title: "JS", isDone: true },
         { id: v1(), title: "ReactJS", isDone: false },
@@ -16,17 +35,21 @@ function App() {
     ]);
 
     function removeTask(id: string) {
-        let filteredTasks = tasks.filter(t => t.id != id);
-        setTasks(filteredTasks);
+        dispatchTasks(removeTaskAC(id))
+       /* let filteredTasks = tasks.filter(t => t.id != id);
+        setTasks(filteredTasks);*/
     }
 
     function addTask(title: string) {
-        let task = { id: v1(), title: title, isDone: false };
+        dispatchTasks(addTaskAC(title))
+      /*  let task = { id: v1(), title: title, isDone: false };
         let newTasks = [task, ...tasks];
-        setTasks(newTasks);
+        setTasks(newTasks);*/
     }
 
-    let [filter, setFilter] = useState<FilterValuesType>("all");
+   /* let [filter, setFilter] = useState<FilterValuesType>("all");*/
+
+    let [filter, dispatchFilter] = useReducer(filterReducer, 'all');
 
     let tasksForTodolist = tasks;
 
@@ -38,7 +61,8 @@ function App() {
     }
 
     function changeFilter(value: FilterValuesType) {
-        setFilter(value);
+        //setFilter(value);
+        dispatchFilter(changeFilterAC(value))
     }
 
 
