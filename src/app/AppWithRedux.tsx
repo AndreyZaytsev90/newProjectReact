8e0bd8461a4f80c15f2f7199f4253d63cefb0866
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2024-11-16 11:22:39 +0300

    SP4 Lesson 13 (refactoring) is done

--- src/app/AppWithRedux.tsx
+++ src/app/AppWithRedux.tsx
@@ -1,10 +1,10 @@
 import { ThemeProvider } from "@mui/material/styles"
 import CssBaseline from "@mui/material/CssBaseline"
-import { getTheme } from "common/theme/getTheme"
 import { Header } from "common/components/Header"
 import { Main } from "./Main"
 import { selectTheme } from "./appSelectors"
 import { useAppSelector } from "common/hooks"
+import { getTheme } from "common/theme"
 
 export const AppWithRedux = () => {
   /*let themeMode = useSelector<RootStateType, ThemeModeType>(state => state.themes)*/
